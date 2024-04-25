66527198ed1843b9a8fedb2a12974f8204d4098b
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-25 08:15:47 -0600

    Time: 273 ms (39.72%) | Memory: 19.2 MB (62.99%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = [1] * (len(nums))
+        result = [1] * len(nums)
 
+        
         prefix = 1
         for i in range(len(nums)):
             result[i] = prefix
             prefix *= nums[i]
 
-        postfix = 1
+        suffix = 1
         for i in range(len(nums) - 1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
-            
+            result[i] *= suffix
+            suffix *= nums[i]
+
         return result
