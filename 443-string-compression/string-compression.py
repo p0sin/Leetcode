14f0556ddf9171ee894a000c57c5c2907a8e2a06
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-25 10:58:43 -0600

    Time: 33 ms (69.99%) | Memory: 12.1 MB (16.81%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,21 +1,23 @@
 class Solution:
     def compress(self, chars):
-        r, i = 0, 0
+        p, i = 0, 0
 
-        while(i < len(chars)):
+        while i < len(chars):
             currChar = chars[i]
             currOcc = 0
-            while ((i < len(chars)) and (chars[i] == currChar)):
-                currOcc += 1
+
+            while (i < len(chars)) and (chars[i] == currChar):
                 i += 1
+                currOcc += 1
 
-            chars[r] = currChar
-            r += 1
-            if(currOcc > 1):
-                currOccStr = str(currOcc)
-                for j in range(len(currOccStr)):
-                    chars[r] = currOccStr[j]
-                    r += 1
+            chars[p] = currChar
+            p += 1
 
-        return r            
+            if currOcc > 1:
+                currOcc = str(currOcc)
+                for n in currOcc:
+                    chars[p] = n
+                    p += 1
+
+        return p           
             
