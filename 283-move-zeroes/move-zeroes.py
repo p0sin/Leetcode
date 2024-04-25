00e880e194670d2730bad68c85783a1c77e98715
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-25 12:37:04 -0600

    Time: 110 ms (87.33%) | Memory: 12.9 MB (67.65%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        index = 0
+        pointer = None
 
         for i in range(len(nums)):
-            # if 
+            if nums[i] == 0:
+                pointer = i
+                break
+
+        if pointer == None:
+            return
+            
+        for i in range(pointer, len(nums)):  
             if nums[i] != 0:
-                if nums[index] != 0:
-                    index += 1
-                else:
-                    temp = nums[index]
-                    nums[index] = nums[i]
-                    nums[i] = temp
-                    index += 1
-        
+                nums[pointer] = nums[i]
+                nums[i] = 0
+                pointer += 1
+
