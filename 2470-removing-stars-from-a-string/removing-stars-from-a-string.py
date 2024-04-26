16ff3c25ddf61a434d6775762b0c2111148026e5
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-26 11:16:57 -0600

    Time: 266 ms (89.80%) | Memory: 14.4 MB (46.84%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+
+        for c in s:
+            if c != "*":
+                stack.append(c)
+            else:
+                stack.pop()
+
+        return "".join(stack)
+        
