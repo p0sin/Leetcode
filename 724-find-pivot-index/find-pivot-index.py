6b79017a63bc103c8d325a4c62b4b81fe1a4cb5f
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-29 10:28:40 -0600

    Time: 101 ms (85.53%) | Memory: 12.7 MB (69.62%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -5,22 +5,15 @@ class Solution(object):
         :rtype: int
         """
         leftSum = 0
-        rightSum = sum(nums[1:])
+        total = sum(nums)
 
-        if leftSum == rightSum:
-            return 0
-
-        for i in range(1, len(nums)):
-            leftSum += nums[i - 1]
-            rightSum -= nums[i]
-
+        for i in range(len(nums)):
+            rightSum = total - nums[i] - leftSum
             if leftSum == rightSum:
                 return i
-            
-        if leftSum == 0:
-            return len(nums) - 1
-        else:
-            return -1
+            leftSum += nums[i]
+        return -1            
+
                 
 
 
