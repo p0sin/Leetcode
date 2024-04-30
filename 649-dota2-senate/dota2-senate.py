5c4fac2454cfcb23a7609dfc9b28a76089836928
p0sin <150264728+p0sin@users.noreply.github.com>
2024-04-30 17:48:19 -0600

    Time: 26 ms (96.44%) | Memory: 12.1 MB (69.33%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        """
+        :type senate: str
+        :rtype: str
+        """
+
+        senate = list(senate)
+        D, R = deque(), deque()
+
+        for i, c in enumerate(senate):
+            if c == "R":
+                R.append(i)
+            else:
+                D.append(i)
+
+        while D and R:
+            dTurn = D.popleft()
+            rTurn = R.popleft()
+
+            if rTurn < dTurn:
+                R.append(dTurn + len(senate))
+            else:
+                D.append(rTurn + len(senate))
+
+        return "Radiant" if R else "Dire"
+        
