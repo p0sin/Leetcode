c787172400b9cfc37bc8500dfe497e82f3857364
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-02 12:51:01 -0600

    Time: 174 ms (84.74%) | Memory: 12.7 MB (88.02%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                return m 
+
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return -1
+
+
+        
