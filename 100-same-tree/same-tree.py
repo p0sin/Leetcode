c7ff32f518a951f9664cd709dca0605d88f5016c
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-04 11:14:59 -0600

    Time: 15 ms (48.29%) | Memory: 11.9 MB (12.56%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -11,13 +11,15 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
+        # Check if both roots and Null, return True
         if not p and not q:
             return True
 
+        # Check if one of those is different:
         if not p or not q or p.val != q.val:
             return False
 
-        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right))
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
 
         
