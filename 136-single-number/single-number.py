5dc86cb0957af5fc13fbab33910a88a30f6eee60
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-06 10:06:21 -0600

    Time: 98 ms (52.60%) | Memory: 13.7 MB (75.99%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0 
+
+        for n in nums:
+            res = n ^ res
+        return res
+
