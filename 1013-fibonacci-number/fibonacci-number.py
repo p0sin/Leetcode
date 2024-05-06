9e69441ef4f38994841544142053cb48def71e9f
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-06 12:24:21 -0600

    Time: 13 ms (73.17%) | Memory: 11.7 MB (38.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,17 +4,30 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # option 1        
+        # cache = {}
 
-        
-        cache = {}
+        # if n in cache:
+        #     return chache[n]
+        # elif n < 2:
+        #     cache[n] = n
+        #     return cache[n]
+        # else:
+        #     cache[n] = self.fib(n - 1) + self.fib(n - 2)
 
-        if n in cache:
-            return chache[n]
-        elif n < 2:
-            cache[n] = n
-            return cache[n]
-        else:
-            cache[n] = self.fib(n - 1) + self.fib(n - 2)
+        #     return cache[n]
 
-            return cache[n]
+        # option 2
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+
+        one, two = 0, 1
+        for num in range(2, n):
+            temp = two
+            two = one + two
+            one = temp
+
+        return one + two
     
