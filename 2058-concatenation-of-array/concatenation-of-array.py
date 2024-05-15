5e148e7ef500cc57351841923e7d40d83928bc48
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-15 13:29:20 -0600

    Time: 53 ms (65.70%) | Memory: 11.7 MB (96.12%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        ans = nums + nums
+
+        return ans
+        
