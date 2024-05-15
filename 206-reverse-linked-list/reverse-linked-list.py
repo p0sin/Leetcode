a54eb592abe531b223b766476a68df9c734a993f
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-15 16:07:44 -0600

    Time: 11 ms (94.42%) | Memory: 13.7 MB (47.89%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -9,15 +9,14 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-
         prev = None
-        current = head
+        cur = head
 
-        while current:
-             nxt = current.next
-             current.next = prev
-             prev = current
-             current = nxt
+        while cur:
+            nextNode = cur.next
+            cur.next = prev
+            
+            prev = cur
+            cur = nextNode
 
         return prev
-        
