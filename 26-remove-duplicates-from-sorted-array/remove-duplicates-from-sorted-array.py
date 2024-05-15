88d4e08e0921a68beb25771d20e33f9c9b3d0dad
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-15 12:39:10 -0600

    Time: 60 ms (50.30%) | Memory: 13.3 MB (29.20%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,24 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        """ APROACH
-        - Create a pointer at index 0 and iterate through the array
-        - If n is a unique number, keep it and move pointer to next index
-        - If it is not unique, hold the pointer at place until a unique value
-        - Return the length of the unique values
-        """
 
-        prev = nums[0]
-        p = 1
-        ans = 1
+        l = 1
         
-        for i in range(1, len(nums)):
-            if nums[i] > prev:
-                nums[p] = nums[i]
-                ans += 1
-                prev = nums[i]
-                p += 1
+        for r in range(1, len(nums)):
+            if nums[r] > nums[r - 1]:
+                nums[l] = nums[r]
+                l += 1
 
-        return ans
+        return l
 
 
