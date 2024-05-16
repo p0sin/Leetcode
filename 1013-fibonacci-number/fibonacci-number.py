aa8090052de245264becc8053ccbe9e806ade055
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-16 17:40:03 -0600

    Time: 15 ms (68.78%) | Memory: 11.7 MB (10.45%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,41 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # OPTION 1 RECURSIVE BACK TRACKING        
-        # if n <= 1:
-        #     return n
-        # return self.fib(n - 1) + self.fib(n - 2)
-
-        # OPTION 2 TOP DOWN WITH MEMOIZATION
-        # memo = {}
-        # if n in memo:
-        #     return memo[n]
-        # elif n <= 1:
-        #     return n
-        # else:
-        #     memo[n] = self.fib(n - 1) + self.fib(n - 2)
-
-        # return memo[n]
-
-        # OPTION 3 BOTTOM-UP WITH TABULATION
-        # if n <= 1:
-        #     return n
-        # tab = [0, 1]
-
-        # for i in range(2, n + 1):
-        #     tab.append(tab[i - 1] + tab[i - 2])
-
-        # return tab[n]
-
-        # OPTION 4 BOTTOM-UP WITH NO MEMORY
         if n <= 1:
             return n
 
-        a, b = 0, 1
+        num1, num2 = 0, 1
 
         for i in range(2, n + 1):
-            temp = b
-            b += a
-            a = temp
-
-        return b
+            res = num1 + num2
+            num1 = num2
+            num2 = res
+        return res
