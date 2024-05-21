6c287bee8a728064ebfa264f9ec629d26b3cde1d
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-21 11:59:28 -0600

    Time: 51 ms (66.53%) | Memory: 16.6 MB (85.45%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,22 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        cur = []
 
-        def helper(i):
-            if sum(cur) > target or i >= len(candidates):
-                return
-            if sum(cur) == target:
+        def helper(i, cur, total):
+            if total == target:
                 ans.append(cur.copy())
                 return
+            if total > target or i >= len(candidates):
+                return
+
             
             # keep including candidates[i]
             cur.append(candidates[i])
-            helper(i)
+            helper(i, cur, total + candidates[i])
 
             # stop including candidates[i]
             cur.pop()
-            helper(i + 1)
+            helper(i + 1, cur, total)
 
-        helper(0)
+        helper(0, [], 0)
         return ans
