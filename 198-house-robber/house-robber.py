0e485231c8343ea95b4eba03f97e2db653aae5cd
p0sin <150264728+p0sin@users.noreply.github.com>
2024-05-28 10:58:24 -0600

    Time: 42 ms (15.82%) | Memory: 16.7 MB (12.38%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,18 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        def memoization(nums, cache, index):
-            if index == len(nums) - 2 or index == len(nums) - 1:
-                return nums[index]
-            if index in cache:
-                return cache[index]
-            if index >= len(nums) - 2:
-                return 0
+        rob1, rob2 = 0, 0
 
-            cache[index] = nums[index] + max(memoization(nums, cache, index + 2), memoization(nums, cache, index + 3))
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
 
-            return cache[index]
-
-        return max(memoization(nums, {}, 0), memoization(nums, {}, 1))
+        return rob2
 
 
