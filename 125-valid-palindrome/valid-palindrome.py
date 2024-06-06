17f87e72f80f33a513430d304a18282fe324f23e
p0sin <150264728+p0sin@users.noreply.github.com>
2024-06-06 10:54:47 -0600

    Time: 49 ms (33.43%) | Memory: 16.9 MB (98.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,26 +1,19 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
 
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+
+            while l < r and not s[l].isalnum():
                 l += 1
-            while r > l and not self.alphaNum(s[r]):
+
+            while l < r and not s[r].isalnum():
                 r -= 1
+
             if s[l].lower() != s[r].lower():
                 return False
-            l, r = l + 1, r - 1
+
+            l += 1
+            r -= 1
+
         return True
-
-    def alphaNum(self, c):
-        return(
-            ord('A') <= ord(c) <= ord('Z') or
-            ord('a') <= ord(c) <= ord('z') or
-            ord('0') <= ord(c) <= ord('9'))
-
-
-        
