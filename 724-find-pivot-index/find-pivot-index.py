1ee78cfb56e0a3a79c909b22deea71186a02328c
p0sin <150264728+p0sin@users.noreply.github.com>
2024-06-10 11:02:05 -0600

    Time: 121 ms (65.88%) | Memory: 17.7 MB (89.42%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        leftSum = 0
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
         total = sum(nums)
 
+        left_sum = 0
         for i in range(len(nums)):
-            rightSum = total - nums[i] - leftSum
-            if leftSum == rightSum:
+            right_sum = total - nums[i] - left_sum
+            if left_sum == right_sum:
                 return i
-            leftSum += nums[i]
-        return -1            
+            left_sum += nums[i]
 
-                
+        return -1
 
-
-
-        
+       
